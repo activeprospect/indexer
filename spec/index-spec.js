c00f77d200bec38ae13c609d26e1afb3d658ef28
decaffeinate <blake.sager@activeprospect.com>
2020-02-18 15:32:10 -0600

    decaffeinate: Convert index-spec.coffee and 2 other files to JS

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,95 +1,119 @@
-assert = require('chai').assert
-Module = require('module')
-fs = require('fs')
-path = require('path')
-index = require('../src/index')
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS205: Consider reworking code to avoid use of IIFEs
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const {
+  assert
+} = require('chai');
+const Module = require('module');
+const fs = require('fs');
+const path = require('path');
+const index = require('../src/index');
 
 
-mod = null
-modulePath = null
-indexFile = null
+let mod = null;
+let modulePath = null;
+let indexFile = null;
 
-beforeEach ->
-  mod = new Module('test', module)
-  modulePath = path.join(__dirname, 'module')
-  indexFile = path.join(modulePath, 'index.coffee')
+beforeEach(function() {
+  mod = new Module('test', module);
+  modulePath = path.join(__dirname, 'module');
+  return indexFile = path.join(modulePath, 'index.coffee');
+});
 
-afterEach ->
-  for f in fs.readdirSync(modulePath)
-    fs.unlinkSync(path.join(modulePath, f)) unless f.match(/(one|two).coffee/)
-
-
-describe 'Index', ->
-
-  describe 'to module', ->
-
-    it 'should export modules by file name', ->
-      index(path.join(__dirname, 'module'), mod)
-      assert.equal mod.exports.one, 'one'
-      assert.equal mod.exports.two, 'two'
+afterEach(() => (() => {
+  const result = [];
+  for (let f of Array.from(fs.readdirSync(modulePath))) {
+    if (!f.match(/(one|two).coffee/)) { result.push(fs.unlinkSync(path.join(modulePath, f))); } else {
+      result.push(undefined);
+    }
+  }
+  return result;
+})());
 
 
-    it 'should return exports', ->
-      indexed = index(modulePath, mod)
-      keys = Object.keys(indexed)
-      assert.deepEqual keys, ['one', 'two']
-      assert.equal indexed.one, require('./module/one')
-      assert.equal indexed.two, require('./module/two')
+describe('Index', function() {
+
+  describe('to module', function() {
+
+    it('should export modules by file name', function() {
+      index(path.join(__dirname, 'module'), mod);
+      assert.equal(mod.exports.one, 'one');
+      return assert.equal(mod.exports.two, 'two');
+    });
 
 
-    it 'should ignore index file', ->
-      fs.writeFileSync(indexFile, 'module.exports = "index"')
-      assert.deepEqual Object.keys(index(modulePath, mod)), ['one', 'two']
+    it('should return exports', function() {
+      const indexed = index(modulePath, mod);
+      const keys = Object.keys(indexed);
+      assert.deepEqual(keys, ['one', 'two']);
+      assert.equal(indexed.one, require('./module/one'));
+      return assert.equal(indexed.two, require('./module/two'));
+    });
 
 
-    it 'should ignore non-script file', ->
-      fs.writeFileSync(path.join(modulePath, 'docs.md'), '## My Docs')
-      assert.deepEqual Object.keys(index(modulePath, mod)), ['one', 'two']
+    it('should ignore index file', function() {
+      fs.writeFileSync(indexFile, 'module.exports = "index"');
+      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
+  });
 
 
-    it 'should support javascript', ->
-      fs.writeFileSync(path.join(modulePath, 'three.js'), 'module.exports = "index";')
-      assert.deepEqual Object.keys(index(modulePath, mod)), ['one', 'three', 'two']
+    it('should ignore non-script file', function() {
+      fs.writeFileSync(path.join(modulePath, 'docs.md'), '## My Docs');
+      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
+  });
 
 
-    it 'should ignore file', ->
-      assert.deepEqual Object.keys(index(modulePath, mod, 'one')), ['two']
+    it('should support javascript', function() {
+      fs.writeFileSync(path.join(modulePath, 'three.js'), 'module.exports = "index";');
+      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'three', 'two']);
+  });
 
 
-    it 'should ignore multiple files', ->
-      assert.deepEqual Object.keys(index(modulePath, mod, 'one', 'two')), []
+    it('should ignore file', () => assert.deepEqual(Object.keys(index(modulePath, mod, 'one')), ['two']));
+
+
+    return it('should ignore multiple files', () => assert.deepEqual(Object.keys(index(modulePath, mod, 'one', 'two')), []));
+});
 
 
 
-  describe 'without module', ->
+  return describe('without module', function() {
 
 
-    it 'should return exports', ->
-      indexed = index(modulePath)
-      keys = Object.keys(indexed)
-      assert.deepEqual keys, ['one', 'two']
-      assert.equal indexed.one, require('./module/one')
-      assert.equal indexed.two, require('./module/two')
+    it('should return exports', function() {
+      const indexed = index(modulePath);
+      const keys = Object.keys(indexed);
+      assert.deepEqual(keys, ['one', 'two']);
+      assert.equal(indexed.one, require('./module/one'));
+      return assert.equal(indexed.two, require('./module/two'));
+    });
 
 
-    it 'should ignore index file', ->
-      fs.writeFileSync(indexFile, 'module.exports = "index"')
-      assert.deepEqual Object.keys(index(modulePath)), ['one', 'two']
+    it('should ignore index file', function() {
+      fs.writeFileSync(indexFile, 'module.exports = "index"');
+      return assert.deepEqual(Object.keys(index(modulePath)), ['one', 'two']);
+  });
 
 
-    it 'should ignore non-script file', ->
-      fs.writeFileSync(path.join(modulePath, 'docs.md'), '## My Docs')
-      assert.deepEqual Object.keys(index(modulePath, mod)), ['one', 'two']
+    it('should ignore non-script file', function() {
+      fs.writeFileSync(path.join(modulePath, 'docs.md'), '## My Docs');
+      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
+  });
 
 
-    it 'should support javascript', ->
-      fs.writeFileSync(path.join(modulePath, 'three.js'), 'module.exports = "index";')
-      assert.deepEqual Object.keys(index(modulePath)), ['one', 'three', 'two']
+    it('should support javascript', function() {
+      fs.writeFileSync(path.join(modulePath, 'three.js'), 'module.exports = "index";');
+      return assert.deepEqual(Object.keys(index(modulePath)), ['one', 'three', 'two']);
+  });
 
 
-    it 'should ignore file', ->
-      assert.deepEqual Object.keys(index(modulePath, 'one')), ['two']
+    it('should ignore file', () => assert.deepEqual(Object.keys(index(modulePath, 'one')), ['two']));
 
 
-    it 'should ignore multiple files', ->
-      assert.deepEqual Object.keys(index(modulePath, 'one', 'two')), []
+    return it('should ignore multiple files', () => assert.deepEqual(Object.keys(index(modulePath, 'one', 'two')), []));
+});
+});
