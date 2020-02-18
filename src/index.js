f1ec923a90967e0b22a8e5b803194ddb8357e048
Blake Sager <blake.sager@activeprospect.com>
2020-02-18 15:30:59 -0600

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,47 +1,38 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS103: Rewrite code to no longer use __guard__
- * DS205: Consider reworking code to avoid use of IIFEs
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const fs = require('fs');
 const path = require('path');
 
-const isDirWithIndexFile = function(dir) {
+const isDirWithIndexFile = function (dir) {
   if (!fs.lstatSync(dir).isDirectory()) { return false; }
   return fs.existsSync(path.join(dir, 'index.js')) || fs.existsSync(path.join(dir, 'index.coffee'));
 };
 
-module.exports = function(dir, mod, ...ignoreModules) {
+module.exports = function (dir, mod, ...ignoreModules) {
+  let target;
 
-  const target =
-    (() => {
-    if (__guard__(mod != null ? mod.constructor : undefined, x => x.name) === 'Module') {
-      if (mod.exports == null) { mod.exports = {}; }
-      return mod.exports;
-    } else {
-      if (mod != null) { ignoreModules.push(mod); }
-      return {};
-    }
-  })();
+  if (mod && mod.constructor && mod.constructor.name === 'Module') {
+    mod.exports = mod.exports || {};
+    target = mod.exports;
+  } else {
+    if (mod) { ignoreModules.push(mod); }
+    target = {};
+  }
 
   // resolve names of modules in the directory
-  let moduleNames = fs.readdirSync(dir).filter(function(f) {
-    const ext = path.extname(f);
-    return (ext === '.js') || (ext === '.coffee') || isDirWithIndexFile(path.join(dir, f));
-  });
+  let moduleNames = fs
+    .readdirSync(dir)
+    .filter(function (f) {
+      const ext = path.extname(f);
+      return ext === '.js' || ext === '.coffee' || isDirWithIndexFile(path.join(dir, f));
+    });
 
   moduleNames = moduleNames.map(moduleName => path.basename(path.basename(moduleName, '.js'), '.coffee'));
 
   // ignore specified files and the index file
-  if (!(ignoreModules.indexOf('index') >= 0)) {
+  if (!(ignoreModules.includes('index'))) {
     ignoreModules.push('index');
   }
 
-  for (let file of Array.from(ignoreModules)) {
+  for (const file of ignoreModules) {
     const fileIndex = moduleNames.indexOf(file);
     if (fileIndex >= 0) {
       moduleNames.splice(fileIndex, 1);
@@ -49,13 +40,9 @@ module.exports = function(dir, mod, ...ignoreModules) {
   }
 
   // export each module
-  for (let name of Array.from(moduleNames)) {
+  for (const name of moduleNames) {
     target[name] = require(path.resolve(dir, name));
   }
 
   return target;
 };
-
-function __guard__(value, transform) {
-  return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
-}
