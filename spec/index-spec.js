500f4fc339133d788c20a49b782b9966e4362c3e
Blake Sager <blake.sager@activeprospect.com>
2020-02-18 15:41:32 -0600

    Finish test cleanup

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -27,7 +27,7 @@ describe('Index', function () {
     it('should export modules by file name', function () {
       index(path.join(__dirname, 'module'), mod);
       assert.equal(mod.exports.one, 'one');
-      return assert.equal(mod.exports.two, 'two');
+      assert.equal(mod.exports.two, 'two');
     });
 
     it('should return exports', function () {
@@ -35,55 +35,63 @@ describe('Index', function () {
       const keys = Object.keys(indexed);
       assert.deepEqual(keys, ['one', 'two']);
       assert.equal(indexed.one, require('./module/one'));
-      return assert.equal(indexed.two, require('./module/two'));
+      assert.equal(indexed.two, require('./module/two'));
     });
 
     it('should ignore index file', function () {
       fs.writeFileSync(indexFile, 'module.exports = "index"');
-      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
+      assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
     });
 
     it('should ignore non-script file', function () {
       fs.writeFileSync(path.join(modulePath, 'docs.md'), '## My Docs');
-      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
+      assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
     });
 
     it('should support javascript', function () {
       fs.writeFileSync(path.join(modulePath, 'three.js'), 'module.exports = "index";');
-      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'three', 'two']);
+      assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'three', 'two']);
     });
 
-    it('should ignore file', () => assert.deepEqual(Object.keys(index(modulePath, mod, 'one')), ['two']));
+    it('should ignore file', function () {
+      assert.deepEqual(Object.keys(index(modulePath, mod, 'one')), ['two']);
+    });
 
-    return it('should ignore multiple files', () => assert.deepEqual(Object.keys(index(modulePath, mod, 'one', 'two')), []));
+    it('should ignore multiple files', function () {
+      assert.deepEqual(Object.keys(index(modulePath, mod, 'one', 'two')), []);
+    });
   });
 
-  return describe('without module', function () {
+  describe('without module', function () {
     it('should return exports', function () {
       const indexed = index(modulePath);
       const keys = Object.keys(indexed);
       assert.deepEqual(keys, ['one', 'two']);
       assert.equal(indexed.one, require('./module/one'));
-      return assert.equal(indexed.two, require('./module/two'));
+      assert.equal(indexed.two, require('./module/two'));
     });
 
     it('should ignore index file', function () {
       fs.writeFileSync(indexFile, 'module.exports = "index"');
-      return assert.deepEqual(Object.keys(index(modulePath)), ['one', 'two']);
+      assert.deepEqual(Object.keys(index(modulePath)), ['one', 'two']);
     });
 
     it('should ignore non-script file', function () {
       fs.writeFileSync(path.join(modulePath, 'docs.md'), '## My Docs');
-      return assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
+      assert.deepEqual(Object.keys(index(modulePath, mod)), ['one', 'two']);
     });
 
     it('should support javascript', function () {
       fs.writeFileSync(path.join(modulePath, 'three.js'), 'module.exports = "index";');
-      return assert.deepEqual(Object.keys(index(modulePath)), ['one', 'three', 'two']);
+      assert.deepEqual(Object.keys(index(modulePath)), ['one', 'three', 'two']);
     });
 
-    it('should ignore file', () => assert.deepEqual(Object.keys(index(modulePath, 'one')), ['two']));
+    it('should ignore file', function () {
+      assert.deepEqual(Object.keys(index(modulePath, 'one')), ['two']);
+    });
 
-    return it('should ignore multiple files', () => assert.deepEqual(Object.keys(index(modulePath, 'one', 'two')), []));
+    it('should ignore multiple files', function () {
+      assert.deepEqual(Object.keys(index(modulePath, 'one', 'two')), []);
+    });
   });
 });
